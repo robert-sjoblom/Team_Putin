7b801ee05b6cef102bf20675b18fdf0d2250936f
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-09-11 17:04:26 +0200

    removed whitespaces.

--- frontend/dashboard/src/index.js
+++ frontend/dashboard/src/index.js
@@ -7,8 +7,5 @@ import './assets/css/icons.css';
 import './assets/css/style.css';
 import registerServiceWorker from './registerServiceWorker';
 
-
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
