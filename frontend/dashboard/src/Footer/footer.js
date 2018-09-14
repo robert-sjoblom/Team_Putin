cdba0f53555cf53eca3e1c529032bca4c3035110
ElTobbo <tobias.ekman@t-time.se>
2018-09-14 10:37:49 +0200

    Footer included

---
+++ frontend/dashboard/src/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        © 2018 Lexa - <span class="d-none d-sm-inline-block"> by Team Putin Sverige AB</span>.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
