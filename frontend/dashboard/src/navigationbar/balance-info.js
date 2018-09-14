5120966c8c6af7ca566d2799ec88b7f2104532c1
Koofii <kristoffer.frejd@gmail.com>
2018-09-14 10:06:16 +0200

    ok

--- frontend/dashboard/src/navigationbar/balance-info.js
+++ frontend/dashboard/src/navigationbar/balance-info.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import Raphael from 'raphael';
+import 'morris.js/morris.css';
+import 'morris.js/morris';
 
 
-const BalanceInfo = () => {
 
-    return (
+
+class BalanceInfo extends React.Component {
+    
+    balance;
+
+    componentDidMount(){
+        
+    }
+
+    constructor() {
+        super();
+        window.Raphael = Raphael;
+    }
+    
+    render(){
+        return (
         <div className="state-graph">
             <div id="header-chart-1"></div>
             <div className="info">Balance $ 2,317</div>
         </div>
-    )
+        )
+    }
 }
 
 export default BalanceInfo;
