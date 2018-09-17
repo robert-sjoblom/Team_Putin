09e694ae95d78898432bf4457cdc1e5b82f4e83e
Andreas Lymalm <andreas.lymalm@gmail.com>
2018-09-17 11:22:32 +0200

    Created basic frontend, without graphs

--- frontend/dashboard/src/wrapper/Wrapper.js
+++ frontend/dashboard/src/wrapper/Wrapper.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import React from 'react';
 import ConciseWrapper from './conciseBoxes/conciseWrapper/conciseWrapper';
+import GraphSectionWrapper from './GraphSectionWrapper/graphSectionWrapper';
 import InboxWrapper from './Inbox/InboxWrapper';
 import OrderWrapper from './LatestOrders/orderWrapper';
 import TransactionWrapper from './LatestTransactions/transactionWrapper';
@@ -16,80 +17,8 @@ const Wrapper = () => {
         </div>
 
         <div className="row">
-          {/* <!-- MONTHLY EARNINGS BOX TO THE LEFT --> */}
-          <div className="col-xl-3">
-            <div className="card m-b-20">
-              <div className="card-body">
-                <h4 className="mt-0 header-title">Monthly Earnings</h4>
-
-                <div className="row text-center m-t-20">
-                  <div className="col-6">
-                    <h5 className="">$56241</h5>
-                    <p className="text-muted ">Marketplace</p>
-                  </div>
-                  <div className="col-6">
-                    <h5 className="">$23651</h5>
-                    <p className="text-muted ">Total Income</p>
-                  </div>
-                </div>
-
-                <div id="morris-donut-example" className="dashboard-charts morris-charts"></div>
-              </div>
-            </div>
-          </div>
-          {/* <!-- END MONTHLY EARNINGS BOX TO THE LEFT -->
-    
-                    <!-- EMAIL SENT BOX MIDDLE --> */}
-          <div className="col-xl-6">
-            <div className="card m-b-20">
-              <div className="card-body">
-                <h4 className="mt-0 header-title">Email Sent</h4>
-
-                <div className="row text-center m-t-20">
-                  <div className="col-4">
-                    <h5 className="">$ 89425</h5>
-                    <p className="text-muted ">Marketplace</p>
-                  </div>
-                  <div className="col-4">
-                    <h5 className="">$ 56210</h5>
-                    <p className="text-muted ">Total Income</p>
-                  </div>
-                  <div className="col-4">
-                    <h5 className="">$ 8974</h5>
-                    <p className="text-muted ">Last Month</p>
-                  </div>
-                </div>
-
-                <div id="morris-area-example" className="dashboard-charts morris-charts"></div>
-              </div>
-            </div>
-          </div>
-          {/* <!-- END EMAIL SENT BOX MIDDLE -->
-    
-                    <!-- MONTHLY EARNING BOX TO THE RIGHT --> */}
-          <div className="col-xl-3">
-            <div className="card m-b-20">
-              <div className="card-body">
-                <h4 className="mt-0 header-title">Monthly Earnings</h4>
-
-                <div className="row text-center m-t-20">
-                  <div className="col-6">
-                    <h5 className="">$ 2548</h5>
-                    <p className="text-muted ">Marketplace</p>
-                  </div>
-                  <div className="col-6">
-                    <h5 className="">$ 6985</h5>
-                    <p className="text-muted ">Total Income</p>
-                  </div>
-                </div>
-
-                <div id="morris-bar-stacked" className="dashboard-charts morris-charts"></div>
-              </div>
-            </div>
-          </div>
-          {/* <!-- END MONTHLY EARNING BOX TO THE RIGHT --> */}
+          <GraphSectionWrapper></GraphSectionWrapper>
         </div>
-        {/* <!-- end row --> */}
 
         <div className="row">
           <InboxWrapper></InboxWrapper>
