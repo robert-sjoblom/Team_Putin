9a98a79f80cd20fd2783c6a62e1440b976366675
Koofii <kristoffer.frejd@gmail.com>
2018-09-18 09:33:58 +0200

    notifications in wrong branch :D

---
+++ frontend/dashboard/src/navigationbar/notifications/notification.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Notification = (props) => {
+
+    return (
+        <a href="javascript:void(0);" className="dropdown-item notify-item">
+            <div className={"notify-icon" + ' ' + props.status}><i className={"mdi" + ' ' + props.type}></i></div>
+            <p className="notify-details">{props.message}<span className="text-muted">{props.messageDummy}</span></p>
+        </a>
+    )
+}
+
+export default Notification;
