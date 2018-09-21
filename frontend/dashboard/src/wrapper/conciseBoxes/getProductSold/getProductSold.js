2aa9c95931622ae38ef7e5ce6287f2d61e95400f
ElTobbo <tobias.ekman@t-time.se>
2018-09-21 11:14:39 +0200

    Onwards

--- frontend/dashboard/src/wrapper/conciseBoxes/getProductSold/getProductSold.js
+++ frontend/dashboard/src/wrapper/conciseBoxes/getProductSold/getProductSold.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Requests from '../../../Request';
 
-const ProductSold = () => {
-    return (
+class ProductSold extends React.Component {
+    state = { length: 0 }
+    componentDidMount() {
+        Requests.post('orders/getSpecificOrders', {
+            status: ['delivered']
+        })
+       .then(req => this.setState({ ...this.state, length: req.length }))
+       .catch(err => console.log(err));
+    }
+
+    render(){
+        return (
         <div className="col-xl-3 col-md-6">
             <div className="card mini-stat bg-primary">
                 <div className="card-body mini-stat-img">
@@ -10,13 +21,14 @@ const ProductSold = () => {
                     </div>
                     <div className="text-white">
                         <h6 className="text-uppercase mb-3">Product Sold</h6>
-                        <h4 className="mb-4">1890</h4>
+                        <h4 className="mb-4">{this.state.length}</h4>
                         <span className="badge badge-info"> +89% </span> <span className="ml-2">From previous period</span>
                     </div>
                 </div>
             </div>
         </div>
     )
+    }
 }
 
 export default ProductSold;
--- frontend/dashboard/src/wrapper/conciseBoxes/getRevenue/getRevenue.js
+++ frontend/dashboard/src/wrapper/conciseBoxes/getRevenue/getRevenue.js
@@ -10,22 +10,22 @@ class Revenue extends React.Component {
     }
     
     render(){
-return (
-<div className="col-xl-3 col-md-6">
-                        <div className="card mini-stat bg-primary">
-                            <div className="card-body mini-stat-img">
-                                <div className="mini-stat-icon">
-                                    <i className="mdi mdi-buffer float-right"></i>
-                                </div>
-                                <div className="text-white">
-                                    <h6 className="text-uppercase mb-3">Revenue</h6>
-                                    <h4 className="mb-4">${this.state.amount}</h4>
-                                    <span className="badge badge-danger"> -29% </span> <span className="ml-2">From previous period</span>
-                                </div>
-                            </div>
-                        </div>
+        return (
+        <div className="col-xl-3 col-md-6">
+            <div className="card mini-stat bg-primary">
+                <div className="card-body mini-stat-img">
+                    <div className="mini-stat-icon">
+                        <i className="mdi mdi-buffer float-right"></i>
                     </div>
-    )
+                    <div className="text-white">
+                        <h6 className="text-uppercase mb-3">Revenue</h6>
+                        <h4 className="mb-4">${this.state.amount}</h4>
+                        <span className="badge badge-danger"> -29% </span> <span className="ml-2">From previous period</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
     }
 }
 
