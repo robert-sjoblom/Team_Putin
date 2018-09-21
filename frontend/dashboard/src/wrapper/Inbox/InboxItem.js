d62f10c57445d06825f2d19af8bbb359f87dd4e9
robert-sjoblom <31652489+robert-sjoblom@users.noreply.github.com>
2018-09-21 09:01:27 +0200

    Merge conflicts (#28)
    * updated inbox item.
    
    * continue work later.
    
    * fixed merge conflicts

--- frontend/dashboard/src/wrapper/Inbox/InboxItem.js
+++ frontend/dashboard/src/wrapper/Inbox/InboxItem.js
@@ -4,7 +4,7 @@ const InboxItem = ({ key, author, message, time}) => {
   return (
     <a href={key} className="text-dark">
       <div className="inbox-item">
-        <div className="inbox-item-img float-left mr-3"><img src="assets/images/users/user-1.jpg" className="thumb-md rounded-circle"
+        <div className="inbox-item-img float-left mr-3"><img src={require('../../assets/images/users/user-1.jpg')} className="thumb-md rounded-circle"
           alt="" /></div>
         <h6 className="inbox-item-author mt-0 mb-1">{author}</h6>
         <p className="inbox-item-text text-muted mb-0">{message}</p>
---
+++ frontend/dashboard/src/wrapper/RecentActivities/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = ({text}) => <h4 className="mt-0 header-title mb-4">{text}</h4>;
+
+export default Header;
